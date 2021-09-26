20a51ca5ab486b14eeb7fbe78ce53a8257435b08
alex3453 <sokolov.uran@yandex.ru>
2021-09-26 23:45:57 +0500

    LocalPageStart

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("hellow")
+from http.server import HTTPServer, CGIHTTPRequestHandler
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+httpd.serve_forever()
