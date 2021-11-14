5d90282699a2374761dedbd8e5c6e48e6b4040ea
Alexey Sokolov <sokolov.uran@yandex.ru>
2021-11-14 12:11:58 +0500

    try6

--- stat_class.py
+++ stat_class.py
@@ -16,33 +16,36 @@ class Stat:
             json.dump(self.stats, f, indent=2)
 
     def update_stat(self, request):
-        ip = request.remote_addr
-        host = request.json['host']
-        path = request.json['pathname']
-        referrer = request.json['referrer']
-        if host not in self.stats.keys():
-            self._create_dict_for_new_host(host)
+        try:
+            ip = request.remote_addr
+            host = request.json['host']
+            path = request.json['pathname']
+            referrer = request.json['referrer']
+            if host not in self.stats.keys():
+                self._create_dict_for_new_host(host)
 
-        host_stat = self.stats[host]
-        host_stat['total_visits_count'] += 1
-        depth = len(list(filter(lambda e: e != '', path.split('/'))))
-        host_stat['average_depth'] = (host_stat['average_depth'] * (host_stat['total_visits_count'] - 1) + depth) / \
-                                     host_stat['total_visits_count']
-        if ip not in host_stat['users_ip_stat'].keys():
-            host_stat['unique_users_count'] += 1
-        if path != '/':
-            if path not in self.stats[host]['paths_visits_stat'].keys():
-                self._create_dict_for_new_path(host, path)
-            path_stat = self.stats[host]['paths_visits_stat'][path]
-            path_stat['total_visits_count'] += 1
-            if ip not in path_stat['users_ip_stat'].keys():
-                path_stat['unique_users_count'] += 1
-            Stat._increment_dict_counter(path_stat['referrer_stat'], referrer)
-            Stat._increment_dict_counter(path_stat['users_ip_stat'], ip)
-        Stat._increment_dict_counter(host_stat['browsers_stat'], request.headers['Sec-Ch-Ua'].split(', ')[1])
-        Stat._increment_dict_counter(host_stat['operation_systems_stat'], request.headers['Sec-Ch-Ua-Platform'])
-        Stat._increment_dict_counter(host_stat['languages_stat'], request.headers['Accept-Language'])
-        Stat._increment_dict_counter(host_stat['users_ip_stat'], ip)
+            host_stat = self.stats[host]
+            host_stat['total_visits_count'] += 1
+            depth = len(list(filter(lambda e: e != '', path.split('/'))))
+            host_stat['average_depth'] = (host_stat['average_depth'] * (host_stat['total_visits_count'] - 1) + depth) / \
+                                         host_stat['total_visits_count']
+            if ip not in host_stat['users_ip_stat'].keys():
+                host_stat['unique_users_count'] += 1
+            if path != '/':
+                if path not in self.stats[host]['paths_visits_stat'].keys():
+                    self._create_dict_for_new_path(host, path)
+                path_stat = self.stats[host]['paths_visits_stat'][path]
+                path_stat['total_visits_count'] += 1
+                if ip not in path_stat['users_ip_stat'].keys():
+                    path_stat['unique_users_count'] += 1
+                Stat._increment_dict_counter(path_stat['referrer_stat'], referrer)
+                Stat._increment_dict_counter(path_stat['users_ip_stat'], ip)
+            Stat._increment_dict_counter(host_stat['browsers_stat'], request.headers['Sec-Ch-Ua'].split(', ')[1])
+            Stat._increment_dict_counter(host_stat['operation_systems_stat'], request.headers['Sec-Ch-Ua-Platform'])
+            Stat._increment_dict_counter(host_stat['languages_stat'], request.headers['Accept-Language'])
+            Stat._increment_dict_counter(host_stat['users_ip_stat'], ip)
+        except:
+            self.stats = {"error": "blin"}
 
 
     def _create_dict_for_new_host(self, host):
